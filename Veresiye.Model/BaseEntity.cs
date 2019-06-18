46e929b2598ce4cf114cad4202877fcbc672e6c3
Ömer Özkan <gs_omer_ozkan@hotmail.com>
2019-06-18 13:51:20 +0300

    first version

---
+++ Veresiye.Model/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veresiye.Model
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    
+    }
+}
---
+++ Veresiye.Service/UserService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veresiye.Data;
+using Veresiye.Model;
+
+namespace Veresiye.Service
+{
+    public class UserService : IUserService
+    {
+        public readonly IUnitOfWork unifOfWork;
+        private readonly IRepository<User> userRepository;
+        public UserService(IUnitOfWork ıunitOfWork,IRepository<User> userRepository)
+        {
+            this.unifOfWork = unifOfWork;
+            this.userRepository = userRepository;
+        }
+        public IEnumerable<User> GetAll()
+        {
+            return userRepository.GetAll();
+        }
+
+        public User Login(string userName, string password)
+        {
+            userName = userName.ToLower();
+            var user = userRepository.Get(x => x.UserName == userName && x.Password == password);
+            return user;
+            
+        }
+
+        public bool Register(string userName, string password, string confirmPassword)
+        {
+            userName = userName.ToLower();
+            //validasyonlar
+           if (password == confirmPassword)
+            {
+                return false;
+            }
+            else if(string.IsNullOrEmpty(userName))
+            {
+                return false;
+            }
+           else
+            {
+                var user = userRepository.Get(x => x.UserName == userName);
+                if (user != null)
+                {
+                    return false;
+                }
+            }
+            var newUser = new User();
+            newUser.UserName = userName;
+            newUser.Password = password;
+            userRepository.Insert(newUser);
+            unifOfWork.SaveChanges();
+            return true;
+        }
+    }
+    public interface IUserService
+    {
+        User Login(string userName, string password);
+        bool Register(string userName, string password, string confirmPassword);
+        IEnumerable <User> GetAll();
+
+    }
+}
